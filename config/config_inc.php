9c5b85cf2605ce53eaf2d60353f9922bb5ff37c9
unknown <fmattosteran9000@gmail.com>
2025-11-18 15:19:21 -0400

    guardar cambios en config_inc.php en cada reinicio de render

---
+++ config/config_inc.php
@@ -0,0 +1,9 @@
+<?php
+$g_hostname               = 'dpg-d4duvaer433s738baq7g-a';
+$g_db_username            = 'mantisuser';
+$g_db_password            = 'tzdfrG064Qe7nYXsWYEhZUNpK9LEjeKG';
+$g_database_name          = 'mantisbtdb';
+$g_db_type                = 'pgsql';
+
+$g_crypto_master_salt     = 'b!Ni>7w&T4Foyjl:';
+?>
